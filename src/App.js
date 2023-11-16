a54d82d3e65f12b8fec4041e4294e675ae73a217
Hatice-Altun <van.altun@outlook.com>
2023-11-16 01:15:18 +0100

    Testimonials

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Testimonials from "./Testimonials/Testimonials";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Testimonials/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+// Testimonials
+const testimonials = [
+    {
+        name: "Hatice Altun",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deleniti eveniet hic id in perspiciatis porro provident voluptate! At dolores ea eveniet ipsam quaerat qui recusandae reprehenderit sapiente suscipit tempora.",
+    },
+    {
+        name: "AltunIT",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deleniti eveniet hic id in perspiciatis porro provident voluptate! At dolores ea eveniet ipsam quaerat qui recusandae."
+    },
+    {
+        name: "Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deleniti eveniet hic id in perspiciatis porro provident voluptate! At dolores ea eveniet ipsam quaerat qui recusandae reprehenderit sapiente suscipit tempora."
+    }
+];
+
+export default {testimonials};
